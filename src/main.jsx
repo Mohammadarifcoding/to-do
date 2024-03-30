423036038353dfa201f08ef4e11ffcf1a8901842
Mohammad Arif <programmermdarif@gmail.com>
2024-03-30 15:47:29 +0600

    Created the banner

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-import Exploring from './Exploring/Exploring';
-
-
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Exploring></Exploring>,
-  },
-]);
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-   <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",'./node_modules/preline/preline.js',],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [ require('preline/plugin'),],
-};
+  plugins: [],
+}
